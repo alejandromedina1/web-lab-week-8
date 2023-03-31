a1c8cf57e2ed87c7f4e871cbd9157403caa20d81
alejandromedina1 <alejandro.medina1@u.icesi.edu.co>
2023-03-31 08:02:36 -0500

    Lab done

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,20 +1,57 @@
-class card extends HTMLElement {
+class Card extends HTMLElement {
     constructor() {
         super()
         this.attachShadow({
             mode: 'open'
         })
+        this.name = this.getAttribute('name')
+        this.url = this.getAttribute('url')
+        this.description = this.getAttribute('description')
+        this.category = this.getAttribute('category')
+        this.collection = this.getAttribute('collection')
+        this.colors = this.getAttribute('color')
+        this.price = this.getAttribute('price')
 
     }
     static get observedAttributes() {
-        return []
+        return ['name', 'url', 'description', 'category', 'collection', 'colors', 'price']
     }
     render() {
         this.shadowRoot.innerHTML = `
-        <link rel = "stylesheet" href = "/src/components/card/style.css"
+        <link rel = "stylesheet" href = "/src/components/card/style.css">
+        <article>
+            <figure>
+                <img src="${this.url}">
+            </figure>
+                <div>
+                    <h1 id ="title"> ${this.name} </h1>
+                    <p class = "paragraph-text"> ${this.description} </p>
+                    <div id="content-chars"> 
+                        <h4> Color: ${this.colors}</h4>
+                        <h3> ${this.price}$</h3>
+                    </div>
+                </div>
+                <div id="footer-card">
+                    <div id="footer-card-left"> 
+                        <h2> ${this.category}</h2>
+                    </div>
+                    <div id="footer-card-right"> 
+                        <h2>${this.collection}</h2>
+                    </div>
+                </div>
+        </article>
         `
+        console.log(this.url)
     }
     connectedCallback() {
         this.render()
     }
+    attributeChangedCallback(propName, oldValue, newValue) {
+        console.log(`attr ${propName} changed`)
+        this[propName] = newValue
+        this.render
+    }
 }
+
+customElements.define('nike-card', Card)
+export default Card
--- src/index.js
+++ src/index.js
@@ -1,2 +1,44 @@
 import "./components/card/card.js"
 
+async function getContent() {
+
+    const content = {
+        method: 'GET',
+        headers: {
+            'content-type': 'application/json'
+        },
+    }
+    const API = await fetch('https://641f59d9f228f1a83eb525fb.mockapi.io/nikeData/catalog', content)
+    const data = await API.json()
+    return data
+}
+const itemList = await getContent()
+
+const cardList = document.getElementById('card-list')
+
+function refreshList() {
+    cardList.innerHTML = ''
+
+    for (const item of itemList) {
+        const element = document.createElement('nike-card')
+        updateItem(element, 'name', item.name)
+        updateItem(element, 'description', item.description)
+        updateItem(element, 'category', item.category)
+        updateItem(element, 'collection', item.collection)
+        updateItem(element, 'price', item.price)
+        updateItem(element, 'colors', item.color)
+        updateItem(element, 'url', item.url)
+        cardList.append(element)
+    }
+}
+
+refreshList()
+
+function updateItem(item, key, value) {
+    item[key] = value
+    item.setAttribute(key, value)
+}
+
+
+console.log(itemList)
+
